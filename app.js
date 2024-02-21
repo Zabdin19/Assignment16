9041f2f1ba57f6ba2f4e4e4d95e792a5cb1dcff2
zain ul abdin <zainjee331@gmail.com>
2024-02-22 00:49:43 +0500

    add files

---
+++ app.js
@@ -0,0 +1,131 @@
+alert("hello world");
+
+document.write("100");
+
+var a = 30;
+
+console.log(typeof a);
+
+var b = "zain";
+
+console.log(b);
+
+chapter no 1
+
+Q1
+
+alert("Error! please enter a valid password.");
+
+Q2
+
+alert("Welcome ti JS Land...\nHappy Coding!");
+
+Q3
+
+alert("Welcome ti JS Land...");
+
+Q4
+
+alert("Happy Coding!");
+
+Q5
+
+alert("Hello... I can run JS through my web browser's console");
+
+chapter no 2
+
+Q1
+
+var username = "Jhone Doe";
+
+var age = 15;
+
+var position = "Certified Mobile Application Development";
+
+alert(username);
+
+alert(age);
+
+alert(position);
+
+Q2
+
+var a = "PIZZA"
+var b = "PIZZ"
+var c = "PIZ"
+var d = "PI"
+var e = "P"
+
+alert(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e )
+
+Q3
+
+var email = "example@example.com";
+
+alert("My email adress is" + email);
+
+var a =
+  "I am trying to learn from the book A smarter \nway to learn javascript";
+
+  alert(a)
+
+document.write("Yah! I Can write HTML content through javascript");
+
+var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+
+alert(a);
+
+var age = 26;
+
+alert("I am " + age + " years old");
+
+var time = "You have visited this site 14 times";
+
+alert(time)
+
+document.write(
+  "My birth year is 1997 <br> Data type of my ddeclared variable is number"
+);
+
+var visitorName = prompt("Please enter your name:");
+
+var productTitle = prompt("Enter the product title:");
+
+var quantity = parseInt(prompt("Enter the quantity you want to order:"));
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
+document.write(
+  "<b>Jhon Doe</b> ordered <b>5 Tshirt</b>(s) on XYZ Clothing store"
+);
+alert("Welcome to XYZ Clothing Store! Explore our latest collection.");
+
+var firstName = "John";
+var lastName = "Doe";
+var email = "example@example.com";
+var phoneNumber = "123-456-7890";
+var password = "Password123";
+
+alert("First Name: " + firstName);
+alert("Last Name: " + lastName);
+alert("Email: " + email);
+alert("Phone Number: " + phoneNumber);
+alert("Password: " + password);
+alert("You're learning JavaScript!");
+
+var myVariableName = "example";
+
+var myVariable;
+myVariable = "Hello, World!";
+
+var teamName = "Awesome Team";
+alert(teamName);
+
+var bestMan = "Charlie";
+bestMan = "John";
+
+var caseQty;
+
+caseQty = 144;
+
+var num = 9; 
